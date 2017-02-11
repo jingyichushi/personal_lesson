c41c37ad1a3253152efa6eb07c0e92142ff2cf56
personal_lesson <lengrongqiu@gmail.com>
2017-02-11 12:11:20 +0800

    增加工具方法，便于进行Console打印回显

---
+++ Java8/Java8Lesson/src/main/java/base/Util.java
@@ -0,0 +1,14 @@
+package base;
+
+/**
+ * Created by lrq on 2017/2/11.
+ */
+public class Util {
+
+
+    public static Object p(String des,Object obj){
+        System.out.println(des+":\t"+obj);
+        return obj;
+    }
+
+}
